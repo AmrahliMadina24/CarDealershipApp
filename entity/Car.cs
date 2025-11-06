399e1d88400b9ee51a32c70515133e6276469085
Ruhin Hasanzade <ruhinhasanzade@gmail.com>
2025-11-07 02:24:26 +0400

    Update car

--- entity/Car.cs
+++ entity/Car.cs
@@ -6,8 +6,38 @@ using System.Threading.Tasks;
 
 namespace CarDealershipApp.entity
 {
-    internal class Car
+    public class Car
     {
+        private static int idCounter = 1;
+
         public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double CostPrice { get; set; }
+        public double SalePrice { get; set; }
+        public bool IsRented { get; set; }
+
+        public Car() { }
+
+        public Car(string brand, string model, int year, double costPrice, double salePrice, bool isRented)
+        {
+            Id = idCounter++;
+            Brand = brand;
+            Model = model;
+            Year = year;
+            CostPrice = costPrice;
+            SalePrice = salePrice;
+            IsRented = isRented;
+        }
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {Id} , Brand: {Brand} , Model: {Model} , Year: {Year} , CostPrice: {CostPrice} , SalePrice: {SalePrice} , isRented: {IsRented}");
+        }
+
+        
+
     }
 }
