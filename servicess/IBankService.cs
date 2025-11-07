f1b7edadef85f51482a226cff046ed49e40cc16e
AmrahliMadina24 <madinava-mpa201@code.edu.az>
2025-11-07 10:48:16 +0400

    Done Bank Service

--- servicess/IBankService.cs
+++ servicess/IBankService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarDealershipApp.entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace CarDealershipApp.servicess
 {
     internal interface IBankService
     {
+        void ShowBalance(Bank bank);
+
+       
+        void AddIncome(Bank bank, double amount);
+
+        
+        bool Withdraw(Bank bank, double amount);
     }
 }
--- servicess/impl/BankServiceImpl.cs
+++ servicess/impl/BankServiceImpl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarDealershipApp.entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,32 @@ using System.Threading.Tasks;
 
 namespace CarDealershipApp.servicess.impl
 {
-    internal class BankServiceImpl
+    internal class BankServiceImpl : IBankService
     {
+        public void ShowBalance(Bank bank)
+        {
+            Console.WriteLine($"Bank balansınız: {bank.Balance} AZN");
+        }
+
+        public void AddIncome(Bank bank, double amount)
+        {
+            bank.Balance += amount;
+            Console.WriteLine($"+{amount} AZN əlavə olundu. Yeni balans: {bank.Balance} AZN");
+        }
+
+        public bool Withdraw(Bank bank, double amount)
+        {
+            if (bank.Balance >= amount)
+            {
+                bank.Balance -= amount;
+                Console.WriteLine($"-{amount} AZN çıxıldı. Yeni balans: {bank.Balance} AZN");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Balansda kifayət qədər vəsait yoxdur.");
+                return false;
+            }
+        }
     }
 }
